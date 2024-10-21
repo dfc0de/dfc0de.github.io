06ba852d37b0944c78e8ffd7a2471f24ba14f394
dfc0de <30250441@blackpool.ac.uk>
2024-10-21 16:23:51 +0100

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,2 +1,5 @@
-//greet everyone
-("Hello peeps");
+//time and date
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
