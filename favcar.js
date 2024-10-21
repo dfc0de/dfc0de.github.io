b1a763e7190574b3bb0fd441d0b98f435da441c5
dfc0de <30250441@blackpool.ac.uk>
2024-10-21 16:41:55 +0100

    babel test

--- favcar.js
+++ favcar.js
@@ -1,5 +1,21 @@
-// Log my favourite car
-const car = {make:"BMW", model:"M5", color:"green"};
+const root = document.querySelector('html')
 
-// Display the above data
-"My favourite car is a " + car.make;
+// Real cursor element
+const cursor = document.createElement('div')
+cursor.classList.add('cursor')
+root.appendChild(cursor)
+
+// Following extra cursor element
+const follower = document.createElement('div')
+follower.classList.add('cursor', 'cursor__follower')
+root.appendChild(follower)
+
+
+root.addEventListener('mousemove', (e) => {
+  setPosition(follower, e)
+  setPosition(cursor, e)
+})
+
+function setPosition(element, e) {
+  element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+}
