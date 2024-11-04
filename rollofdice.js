28608b5f14d78143d59d5c5043bc3d0ad3e72025
dfc0de <30250441@blackpool.ac.uk>
2024-11-04 16:54:02 +0000

    diceOutcome addition, JS

--- rollofdice.js
+++ rollofdice.js
@@ -1,4 +1,9 @@
 function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('diceRoll').innerText = `${dice}`;
+    const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
+    if (dice === 12) {
+        diceOutcome.innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
+    } else {
+        diceOutcome.innerText = `You rolled a ${dice}. Try again!`;
+    }
 }
