5243124ea4e4e218b8ea66e7d7c685341752797a
dfc0de <30250441@blackpool.ac.uk>
2024-11-04 17:00:13 +0000

    dice roll JS

--- rollofdice.js
+++ rollofdice.js
@@ -2,8 +2,8 @@ function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
     const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
     if (dice === 12) {
-        diceresultDiv.innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
+        document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
     } else {
-        diceresultDiv.innerText = `You rolled a ${dice}. Try again!`;
+        document.getElementById('diceResult').innerText = `You rolled a ${dice}. Try again!`;
     }
 }
