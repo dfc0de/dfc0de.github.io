0724e2a96d4d4df2537f45690148b1397843ff56
dfc0de <30250441@blackpool.ac.uk>
2024-11-04 16:41:08 +0000

    removed the text string from JS

--- rollofdice.js
+++ rollofdice.js
@@ -1,4 +1,4 @@
 function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
-    document.getElementById('diceRoll').innerText = `wahoo it's... ${dice} !!`;
+    document.getElementById('diceRoll').innerText = `${dice}`;
 }
