e2f99e77e2c6830729d32cdc547828a41da09b9f
dfc0de <30250441@blackpool.ac.uk>
2024-11-04 16:57:53 +0000

    JS change, div

--- rollofdice.js
+++ rollofdice.js
@@ -2,8 +2,8 @@ function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
     const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
     if (dice === 12) {
-        diceOutcome.innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
+        diceresultDiv.innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
     } else {
-        diceOutcome.innerText = `You rolled a ${dice}. Try again!`;
+        diceresultDiv.innerText = `You rolled a ${dice}. Try again!`;
     }
 }
