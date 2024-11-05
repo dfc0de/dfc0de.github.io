06b4464e1da08de38019d4f8b57735ab1c196157
dfc0de <30250441@blackpool.ac.uk>
2024-11-05 09:08:35 +0000

    prize, js

--- rollofdice.js
+++ rollofdice.js
@@ -1,6 +1,5 @@
 function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
-    const prize = <a href="https://www.example.com" target="_blank">Visit Example Website</a>
     const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
     if (dice === 12) {
         document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a ${prize}! 🎉`;
