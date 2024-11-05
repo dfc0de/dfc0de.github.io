c57c27be9b0045497dac2f8c96cd690b48f47122
dfc0de <30250441@blackpool.ac.uk>
2024-11-05 09:13:41 +0000

    commented code, included prize URL

--- rollofdice.js
+++ rollofdice.js
@@ -1,8 +1,13 @@
+// this is the link to the prize you win when rolling a 12
+const prize = "https://www.google.com";
+
+//this is the dice roll function
 function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
     const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
     if (dice === 12) {
-        document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a ${prize}! 🎉`;
+        const prizeLink = `<a href="${prize}" target="_blank">prize</a>`;
+        document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a ${prizeLink}! 🎉`;
     } else {
         document.getElementById('diceResult').innerText = `You rolled a ${dice}. Try again!`;
     }
