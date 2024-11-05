f8bc8f7de4b2abb7142b62bb6c6b110d06b5797d
dfc0de <30250441@blackpool.ac.uk>
2024-11-05 09:07:15 +0000

    link url, attempt

--- rollofdice.js
+++ rollofdice.js
@@ -1,8 +1,9 @@
 function rollOfDice() {
     const dice = Math.floor(Math.random() * 12) + 1;
+    const prize = <a href="https://www.example.com" target="_blank">Visit Example Website</a>
     const diceOutcome = document.getElementById('diceRoll').innerText = `${dice}`;
     if (dice === 12) {
-        document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a prize! 🎉`;
+        document.getElementById('diceResult').innerText = `You rolled a ${dice}! Congratulations, you won a ${prize}! 🎉`;
     } else {
         document.getElementById('diceResult').innerText = `You rolled a ${dice}. Try again!`;
     }
